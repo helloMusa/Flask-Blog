fbfea54d61f62987befc32571273c7b5b069a9b6
helloMusa <esquitoban@gmail.com>
2019-11-10 21:07:02 -0500

    Added templates, css

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,30 @@
-from flask import Flask, escape, request
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+	{
+		'author': 'Musa Ali',
+		'title': 'Blog Post 1',
+		'content': 'First post content',
+		'date_posted': 'November 10th, 2019'
+	},
+	{
+		'author': 'John Smith',
+		'title': 'Blog Post 2',
+		'content': 'Second post content',
+		'date_posted': 'November 11th, 2019'
+	}
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-	return "<h1>About Page</h1>"
+	return render_template('about.html', title=about)
 
 if __name__ == '__main__':
 	app.run(debug=True)
